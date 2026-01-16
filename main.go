78f267202a2922976df3156929fca1d9df71a1f1
opkhan27 <opkhan27@umd.edu>
2026-01-16 02:16:35 -0500

    Mock container metadata

--- main.go
+++ main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"api_gateway/internal/proxy"
+	"api_gateway/internal/router"
 	"api_gateway/internal/server"
-	"net/http"
-	"net/http/httputil"
+	"fmt"
 )
-
-func proxyHandler(p *httputil.ReverseProxy) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		p.ServeHTTP(w, r)
-	}
-}
 
 func main() {
 	s := server.GetServer()
-	http.HandleFunc("/", proxyHandler(proxy.GetProxy("http://localhost:9000")))
+
+	mockContainers := [2]router.DockerLabels{
+		{"localhost:8001", "/home"},
+		{"localhost:8002", "/home_two"},
+	}
+
+	routes := []router.Route{
+		{
+			Host:       "localhost",
+			PathPrefix: "/home",
+			Proxy:      proxy.GetProxy("http://localhost:8001"),
+		},
+		{
+			Host:       "localhost",
+			PathPrefix: "/home_two",
+			Proxy:      proxy.GetProxy("http://localhost:8002"),
+		},
+	}
+
+	for _, route := range mockContainers {
+		fmt.Println(route.Host + route.PathPrefix)
+	}
+
+	rt := router.NewRouter(routes)
+	s.Handler = rt
+
 	err := s.ListenAndServe()
 	if err != nil {
 		return
 	}
+
 }
