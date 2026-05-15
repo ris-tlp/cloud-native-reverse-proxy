dc813651e3523dda7bd1bc3c6c38e348a1894cda
Omar Pervez Khan <omarkhantlp@gmail.com>
2026-05-14 20:23:28 -0400

    feat: default reverse proxy config
    Signed-off-by: Omar Pervez Khan <omarkhantlp@gmail.com>

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -1,2 +1,43 @@
 // Package proxy
 package proxy
+
+import (
+	"log/slog"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+)
+
+func New(target *url.URL) *httputil.ReverseProxy {
+	return &httputil.ReverseProxy{
+		Rewrite: func(pr *httputil.ProxyRequest) {
+			pr.SetURL(target)
+			pr.SetXForwarded()
+			pr.Out.Host = target.Host
+		},
+		Transport:    defaultTransport(),
+		ErrorHandler: defaultErrorHandler,
+	}
+}
+
+func defaultTransport() *http.Transport {
+	return &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   5 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   10,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   5 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+}
+
+func defaultErrorHandler(w http.ResponseWriter, req *http.Request, err error) {
+	slog.Error("proxy error", "host", req.Host, "err", err)
+	http.Error(w, "backend unavailable", http.StatusBadGateway)
+}
