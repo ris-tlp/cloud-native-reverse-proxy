c89620d1cab6f70d217fb89426442403a21e6d27
Omar Pervez Khan <omarkhantlp@gmail.com>
2026-06-01 01:00:01 -0400

    feature: central configuration toml for the proxy

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.6.0 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/containerd/errdefs v1.0.0 // indirect
 	github.com/containerd/errdefs/pkg v0.3.0 // indirect
---
+++ internal/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"errors"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	Server    ServerConfig    `toml:"server"`
+	Providers ProvidersConfig `toml:"providers"`
+}
+
+type ServerConfig struct {
+	Port int `toml:"port"`
+}
+
+type ProvidersConfig struct {
+	Docker DockerConfig `toml:"docker"`
+}
+
+type DockerConfig struct {
+	Enabled bool `toml:"enabled"`
+}
+
+func defaults() Config {
+	return Config{
+		Server: ServerConfig{Port: 8080},
+	}
+}
+
+func Load(path string) (*Config, error) {
+	cfg := defaults()
+	_, err := toml.DecodeFile(path, &cfg)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return &cfg, nil
+		}
+		return nil, err
+	}
+	return &cfg, nil
+}
